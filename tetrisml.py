c30e4ae2d98d8457a2e6e9cbd2b380daf8a1e4d5
moose16 <matthewting16@gmail.com>
2025-06-01 11:25:46 -0400

    Update tetrisml.py

--- tetrisml.py
+++ tetrisml.py
@@ -1,1 +1,16 @@
-print("wojdawd")
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Blank Window")
+root.geometry("400x300")
+
+canvas = tk.Canvas(root, width=400, height = 300, bg="white")
+canvas.pack()
+
+canvas.create_rectangle(50,50,150,150,fill="green")
+canvas.create_oval(200,50,300,150,fill="blue")
+
+canvas.create_line(50,200,300,250,fill="red",width=3)
+
+root.mainloop()
+
