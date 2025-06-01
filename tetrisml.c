8d23a322f9d297b08cd5e6f9ce0098214a6cb95b
moose16 <matthewting16@gmail.com>
2025-06-01 11:03:30 -0400

    e

--- tetrisml.c
+++ tetrisml.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int main() {
-<<<<<<< HEAD
-    printf("I am playing tetris hohoho dawlhjiawdhlahdahdlahwlidhai");
-=======
-    printf("I am playing tetris hohoho hihihi");
-    printf("I am playing tetris hohoho hihihi");
->>>>>>> fe27aa3a04cb5b675d732d27f9eea4dc4b8d4be6
-    return 0;
-}
+int main(int argc, char *argv[]) {
+    if (argc==1){
+        system("start cmd /K \"snake.exe argument\"");
+    }
+    if (argc > 1){
+        
+    
+        return 0;
+    }
