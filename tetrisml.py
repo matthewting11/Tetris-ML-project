e6ecdff2ca7f16777453c763e092e3f12084855d
moose16 <matthewting16@gmail.com>
2025-06-01 11:43:04 -0400

    Update tetrisml.py

--- tetrisml.py
+++ tetrisml.py
@@ -2,15 +2,20 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("Blank Window")
-root.geometry("400x300")
+root.geometry("576x632")
 
-canvas = tk.Canvas(root, width=400, height = 300, bg="white")
+canvas = tk.Canvas(root, width=576, height=632, bg="black")
 canvas.pack()
 
-canvas.create_rectangle(50,50,150,150,fill="green")
-canvas.create_oval(200,50,300,150,fill="blue")
+block_size = 16
+cols = 576 // block_size
+rows = 632 // block_size
 
-canvas.create_line(50,200,300,250,fill="red",width=3)
+def draw_block(x,y,color):
+    canvas.create_rectangle(x,y,x+block_size,y+block_size, fill=color, outline="white")
+for row in range(rows):
+    for col in range(cols):
+        color = "gray"
+        draw_block(col * block_size, row * block_size, color)
 
 root.mainloop()
-
