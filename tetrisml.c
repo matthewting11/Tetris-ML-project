9edbd78fd10e0692bd6647bd7e4d7dc741223671
moose16 <matthewting16@gmail.com>
2025-06-01 11:12:15 -0400

    e

--- tetrisml.c
+++ tetrisml.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-int main(int argc, char *argv[]) {
-    if (argc==1){
-        system("start cmd /K \"snake.exe argument\"");
-    }
-    if (argc > 1){
-        
-    
-        return 0;
-    }
+print("hello world")
