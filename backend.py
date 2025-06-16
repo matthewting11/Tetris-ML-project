72d3f96e2ac641a8d7479f61bded991ed7b1336f
moose16 <matthewting16@gmail.com>
2025-06-16 16:49:22 -0400

    lol

--- backend.py
+++ backend.py
@@ -1,4 +1,3 @@
-print("hello")
 import random
 
 
--- tetrisml.py
+++ tetrisml.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 root = tk.Tk()
 root.title("Tetris Game")
@@ -7,11 +8,56 @@ root.geometry("576x632")
 canvas = tk.Canvas(root, width=576, height=632, bg="gray")
 canvas.pack()
 
+current_piece = None
+
+def pieceidtoblocks(pieceid):
+    if pieceid == 0:
+        return [[0,0]]
+    elif pieceid == 1:
+        return [[0, 0], [1, 0], [0, 1], [1, 1]] #Square
+    elif pieceid == 2:
+        return [[-1, 0], [0, 0], [1, 0], [2, 0]] #Long
+    elif pieceid == 3:
+        return [[0, 0], [1, 0], [-1, 1], [0, 1]] #S
+    elif pieceid == 4:
+        return [[-1, 0], [0, 1], [0, 0], [1, 1]] #Z
+    elif pieceid == 5:
+        return [[0, 1], [-1, 1], [1, 1], [0, 0]] #T
+    elif pieceid == 6:
+        return [[-1, 0], [-1, 1], [0, 1], [1, 1]] #L
+    elif pieceid == 7:
+        return [[-1, 0], [0, 0], [1, 0], [1, 1]] #J
+    else:
+        return [[]] # FALLBACK CASE, SHOULD NOT HAPPEN
+    
 block_size = 16
 cols = 11
 rows = 27 
 start_x = 200
 start_y = 100
+
+
+
+def start_game():
+    global paused, start_button, current_piece
+    paused = False
+    start_button.destroy()
+    
+    random_piece_id = random.randint(1,7)
+    center_col = cols//2
+    current_piece = {
+    "blocks": [[center_col+ dx, dy] for dx, dy in pieceidtoblocks(random_piece_id)],
+    "color": "cyan"
+    }
+
+
+    draw_grid()
+    draw_game_UI()
+    update_screen()
+    update_block()
+
+start_button = tk.Button(root, text="Start Game", font=("Courier", 16), command=start_game)
+canvas.create_window(288,316, window=start_button)
 
 
 paused = False
@@ -58,32 +104,28 @@ def draw_game_UI():
     canvas.create_text((level_box_x0+level_box_x1)/2, level_box_y0 +20, text="Level:", fill = "white")
     canvas.create_text((level_box_x0 + level_box_x1)/2, level_box_y0 +60, text=level, fill="white", font = ("Courier",24))
 
-
-
-def draw_falling_block():
-    col = block["x"]
-    row = block["y"]
-    x = start_x + col * block_size
-    y = start_y + row * block_size
-    draw_block(x, y, "red")
+def update_block():
+    if not current_piece or paused:
+        root.after(500, update_block)
+        return
+    if not paused:
+        for i in range(len(current_piece["blocks"])):
+            current_piece["blocks"][i][1] += 1
+        update_screen()
+    root.after(500, update_block)
 
 
 def update_screen():
     canvas.delete("all")
     draw_grid()
     draw_game_UI()
-    draw_falling_block()
-
-
-def update_block():
-    if not paused:
-        if block["y"] < rows -1:
-            block["y"] +=1
-        else:
-            block["y"]=0
-        update_screen()
-    root.after(500, update_block)
-
+    if not current_piece:
+        return
+    if current_piece:
+        for dx,dy in current_piece["blocks"]:
+            x = start_x + dx*block_size
+            y = start_y + dy*block_size
+            draw_block(x,y,current_piece["color"])
 
 def toggle_pause(event=None):
     global paused
@@ -97,9 +139,9 @@ def toggle_pause(event=None):
         canvas.delete("pause")
         update_screen()
 
+
 root.bind("<Escape>", toggle_pause)
 
-update_screen()
 root.after(500, update_block)
 
 root.mainloop()
